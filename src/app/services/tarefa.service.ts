679220f1696b3176d2de40b425295052d8e59512
prof-fernando <fernando.oliveira@iffarroupilha.edu.br>
2025-05-05 20:33:00 -0300

    update fontes feitos em aula

--- src/app/services/tarefa.service.ts
+++ src/app/services/tarefa.service.ts
@@ -26,20 +26,30 @@ export class TarefaService {
   tarefas: Tarefa[] = [];
   constructor(private firestore: Firestore) {}
 
-  addTarefa(novaTarefa: Tarefa) {
+  async addTarefa(novaTarefa: Tarefa) : Promise<string> {
     //this.tarefas.push(novaTarefa);
     const ref = collection(this.firestore, 'tarefas');
-    addDoc(ref, novaTarefa).catch((erro) => {
-      console.log('erro:', erro);
-    });
+    
+    if(novaTarefa.id){
+      // atualizacao
+      // pega a tarefa especifica aseada em sua chave
+      const tarefa = doc( ref, novaTarefa.id );
+      updateDoc(tarefa,{ novaTarefa});
+      return Promise.resolve(novaTarefa.id);
+    }else{
+      // inserção / inclusão
+      const novo = 
+      await addDoc(ref, novaTarefa).catch((erro) => {
+        console.log('erro:', erro);
+        return Promise.reject(null);
+      });
+
+      return Promise.resolve(novo.id);
+    }
+    
+    
   }
 
-  add(tituloTarefa: string) {
-    this.tarefas.push({
-      titulo: tituloTarefa,
-      concluida: false,
-    });
-  }
   listar(): Observable<Tarefa[]> {
     const ref = collection(this.firestore, 'tarefas');
     return collectionData(ref, { 'idField': 'id' }) as Observable<Tarefa[]>;
